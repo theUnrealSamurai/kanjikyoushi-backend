037a0f64c7527849d7c3a3c259bbc26968032e34
samurai <roshan.saravanan001@gmail.com>
2024-12-16 15:32:28 +0900

    Modify: to fetch from db instead of genrating ichiran and translation

--- server/type/utils/fetch_sentence.py
+++ server/type/utils/fetch_sentence.py
@@ -5,11 +5,11 @@ from .utils import *
 def fetch_practice_sentence(kanji: str, unknownkanji: str, maxrow: int):
     index = search_max_kanji_match(sparse_matrix, sparse_matrix_kanji, kanji, unknownkanji, maxrow)
     row = fetch_psql_row(index)
-    ichiran_data = get_ichiran_data(row[2])
+    ichiran_data = row[7]
 
     return {
         "japanese": row[2],
-        "english": translate(row[2]),
+        "english": row[6],
         "romaji": ichiran_data.split("\n\n*")[0].strip(),
         "kanji_data": fetch_kanji_data(re.findall(r'[\u4e00-\u9faf]', row[2])),
         "definitions": ichiran_data.split("\n\n*")[1:],
@@ -20,14 +20,14 @@ def fetch_practice_sentence(kanji: str, unknownkanji: str, maxrow: int):
 def fetch_revision_sentence(due_kanji, unknown_kanji, maxrow):
     index = search_max_kanji_match(sparse_matrix, sparse_matrix_kanji, due_kanji, unknown_kanji, maxrow)
     row = fetch_psql_row(index)
-    ichiran_data = get_ichiran_data(row[2])
-    
+    ichiran_data = row[7]
+        
     if not contains_kanji(row[2], due_kanji):
         return None
 
     return {
         "japanese": row[2],
-        "english": translate(row[2]), 
+        "english": row[6],
         "romaji": ichiran_data.split("\n\n*")[0].strip(),
         "kanji_data": fetch_kanji_data(re.findall(r'[\u4e00-\u9faf]', row[2])),
         "definitions": ichiran_data.split("\n\n*")[1:],
